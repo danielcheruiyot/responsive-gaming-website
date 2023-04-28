2c2621c47864d4acc35a5dbebc5e820147d1c924
danielcheruiyot <danielcheruiyot88@gmail.com>
2023-04-28 15:09:21 +0300

    Front hero page done

--- main.js
+++ main.js
@@ -20,4 +20,37 @@ const sr = ScrollReveal ({
 
 sr.reveal('.hero-text',{delay:200, origin:'top'});
 sr.reveal('.hero-img',{delay:700, origin:'top'});
+sr.reveal('.icons',{delay:900, origin:'left'});
+sr.reveal('.scroll-down',{delay:900, origin:'right'});
 
+
+
+
+/*hover animation */
+function animateBtnFill(btnFill, translateY, duration) {
+    requestAnimationFrame(() => {
+        btnFill.animate(
+            {
+                transform: `translate(-50%, ${translateY}%)`,
+            },
+
+            {duration, fill:'forwards', easing: 'ease'}
+        )
+    })
+}
+
+
+const buttons = document.querySelectorAll('.ctaa');
+buttons.forEach((btn) => {
+    const btnFill = btn.querySelectorAll('.btn-fill')
+
+    btn.addEventListener('mouseenter', () => {
+        animateBtnFill(btnFill, 50, 0)
+
+        animateBtnFill(btnFill, -50, 850)
+    })
+
+    btn.addEventListener('mouseleave', () => {
+        animateBtnFill(btnFill, -150,850)
+    })
+})
